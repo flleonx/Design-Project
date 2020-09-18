ebd12c6b1210fa08f1d5d333dcca6fcebcaa8ccc
flleonx <felipeleonn99@gmail.com>
2020-09-18 08:20:18 -0500

    Version 18/09/2020

--- Server.js
+++ Server.js
@@ -9,7 +9,7 @@ var longitud ='x2';
 var stamptime ='x3';
 const fs= require('fs');
 
-app.set('port', 10001);
+app.set('port', 24000);
 
 //ENVIAR ARCHIVOS AL REQUEST
 app.get('/',function(req,res){
@@ -93,7 +93,7 @@ server.on('listening', () => {
 });
 
         
-server.bind(20001);
+server.bind(24200);
         
 
 app.listen(app.get('port'), () => {
